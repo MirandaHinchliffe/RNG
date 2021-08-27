ad8b43c0f469e0ceb5d9c3c44463a992e9b24f58
Miranda Hinchliffe <70153183+MirandaHinchliffe@users.noreply.github.com>
2021-08-27 09:13:45 -0400

    Add files via upload

---
+++ RANDOM # GUESSER/RNG.py
@@ -0,0 +1,18 @@
+from array import array
+import random as r
+import random as m
+
+number = 0
+
+randomNumber = r.randint(1, 100)
+randomNumber = m.randint(1, 100)
+
+if number > 100:
+    print("Number must be 100 or less than")
+if number < 1:
+    print("Number must be 1 or greater than")
+
+
+print("Computer takes a guess")
+ 
+
